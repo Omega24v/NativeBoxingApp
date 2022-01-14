aa01fd00a1ceea924b12bdf6c48888165a1ab201
omega24v <omega24v1993@gmail.com>
2022-01-14 13:07:41 +0200

    import reducers from BoxingApp

--- store/rootReducer.js
+++ store/rootReducer.js
@@ -1,6 +1,6 @@
-import {combineReducers, createStore} from "redux";
-import reducers from './reducers'
+import { combineReducers, createStore } from 'redux';
+import reducers from './reducers';
 
 const rootReducer = combineReducers(reducers);
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer);
