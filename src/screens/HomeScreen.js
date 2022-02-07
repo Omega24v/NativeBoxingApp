4000fba224796601395296861ab86d698c0c32ef
omega24v <omega24v1993@gmail.com>
2022-02-07 19:54:09 +0200

    add tailwind react native classnames

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,18 @@
-import React, { View, Text } from 'react-native';
-import TimerList from '../components/timer/TimerList';
-import tw from 'tailwind-rn';
+import { View, Text } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { useSelector } from 'react-redux';
 import Timer from '../components/timer/Timer';
+import TimerList from '../components/timer/TimerList';
 
 const HomeScreen = () => {
   const timerState = useSelector((state) => state.timerReducer);
 
   return (
-    <SafeAreaView style={tw(`h-full justify-between`)}>
-      <View style={tw(`items-center`)}>
-        <Text style={tw(`text-3xl mt-4 mb-10 uppercase font-bold`)}>
+    <SafeAreaView style={tw`h-full justify-between`}>
+      <View style={tw`items-center`}>
+        <Text style={tw`text-3xl mt-4 mb-10 uppercase font-bold`}>
           {timerState.currTimer.name}
         </Text>
         <Timer />
